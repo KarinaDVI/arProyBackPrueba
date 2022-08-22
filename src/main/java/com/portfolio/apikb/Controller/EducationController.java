7dbb37d956bbe7689b4c9dbffd77af183045ae50
@KarinaDVI <kbbouza@gmail.com>
2022-08-22 00:25:01 -0300

    making even better

--- src/main/java/com/portfolio/apikb/Controller/EducationController.java
+++ src/main/java/com/portfolio/apikb/Controller/EducationController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://portfoliofrontkb.web.app")
+@CrossOrigin
 @RestController
 @RequestMapping("/apikb/education")
 
